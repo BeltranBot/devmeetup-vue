160c12560396d2bedc23565613c0ce6c3795de9a
Carlos Beltran <carlos.beltran@pixeling.co>
2017-07-20 11:58:29 -0500

    updated to latest video https://www.youtube.com/watch?v=at6QjSwKOuA

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,55 @@
 <template>
-  <div>
-    <p>The home page</p>
-  </div>
-
+  <v-container>
+    <v-layout row wrap mb-2>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+		<v-layout row wrap mt-2>
+			<v-flex xs12>
+				<v-carousel>
+					<v-carousel-item
+						v-for="meetup in meetups"
+						:src="meetup.imageUrl"
+						:key="meetup.id">
+            <div class="title">
+              {{meetup.title}}
+            </div>
+					</v-carousel-item>
+				</v-carousel>
+			</v-flex>
+		</v-layout>
+    <v-layout row wrap mb-2>
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      meetups: [
+        { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg', id: 'afajfjadfaadfa323', title: 'Meetup in New York' },
+        { imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Paris_-_Blick_vom_gro%C3%9Fen_Triumphbogen.jpg', id: 'aadsfhbkhlk1241', title: 'Meetup in Paris' }
+      ]
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="css" scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    border-color: rgba(0, 0, 0, 0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
 </style>
