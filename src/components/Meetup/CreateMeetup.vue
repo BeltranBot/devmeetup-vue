e94c35e857f1bdaf5c1aa968961f0682e46905ac
Carlos Beltran <carlos.beltran@pixeling.co>
2017-10-16 13:02:14 -0500

    Updated to https://www.youtube.com/watch?v=J2Wp4_XRsWc

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -37,13 +37,14 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-              <v-text-field
-                name="imageUrl"
-                label="Image URl"
-                id="image-url"
-                v-model="imageUrl"
-                required>
-              </v-text-field>
+              <v-btn raised class="primary" @click="onPickFile">Upload Image </v-btn>
+              <input
+                type="file"
+                style="display:none"
+                ref="fileInput"
+                accpet="image/*"
+                @change="onFilePicked"
+              >
             </v-flex>
           </v-layout>
           <v-layout row>
@@ -102,15 +103,16 @@
         imageUrl: '',
         description: '',
         date: moment().format('YYYY-MM-DD'),
-        time: moment().format('h:mm')
+        time: moment().format('h:mm'),
+        image: null
       }
     },
     computed: {
       formIsValid () {
         return this.title !== '' &&
-                this.location !== '' &&
-                this.imageUrl !== '' &&
-                this.description !== ''
+          this.location !== '' &&
+          this.imageUrl !== '' &&
+          this.description !== ''
       }
     },
     methods: {
@@ -118,15 +120,34 @@
         if (!this.formIsValid) {
           return
         }
+        if (!this.image) {
+          return
+        }
         const meetupData = {
           title: this.title,
           location: this.location,
-          imageUrl: this.imageUrl,
+          image: this.image,
           description: this.description,
           date: moment().format(this.date + ' ' + this.time)
         }
         this.$store.dispatch('createMeetup', meetupData)
         this.$router.push('/meetups')
+      },
+      onPickFile () {
+        this.$refs.fileInput.click()
+      },
+      onFilePicked (event) {
+        const files = event.target.files
+        let filename = files[0].name
+        if (filename.lastIndexOf('.') <= 0) {
+          return alert('Please add a valid file!')
+        }
+        const fileReader = new FileReader()
+        fileReader.addEventListener('load', () => {
+          this.imageUrl = fileReader.result
+        })
+        fileReader.readAsDataURL(files[0])
+        this.image = files[0]
       }
     }
   }
